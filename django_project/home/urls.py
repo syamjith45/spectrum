04c5bc45bc9029f692ad0b947856d5bfa6cbb03e
syamjith45 <97729113+syamjith45@users.noreply.github.com>
2023-11-09 15:18:47 +0530

    Delete django_project directory

--- django_project/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("create/", views.create, name='create'),
-    path("edit/", views.edit,name='edit'),
-    path("list/", views.list, name='list')
-
-]
--- django_project/products/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>HELLO</h1>")
-def about(request):
-    return HttpResponse("<h1>about</h1>")
-def contact(request):
-    return HttpResponse("<h1>contact</h1>")
